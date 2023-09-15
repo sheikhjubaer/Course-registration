ab31483f9a9eed7cb83af5c3e17777456e990e69
sheikhjubaer <mohammadjubaer01@gmail.com>
2023-09-15 10:44:18 +0600

    added API with fake data in a file named course.json and created header layout with css background color

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+    return (
+        <header className="flex justify-center items-center p-4 max-w-7xl mx-auto mb-8">
+            <h1 className='text-4xl font-bold'>Course Registration</h1>
+        </header>
+    );
+};
+
+export default Header;
